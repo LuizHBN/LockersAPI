af0b138f3c61f75fdcdd93ead485595cba78efe0
Luiz Henrique Netto <101609888+LuizHBN@users.noreply.github.com>
2025-06-20 17:28:20 -0300

    New feature: User get by id

--- src/main/java/com/inertia/lockersapi/api/controller/UserController.java
+++ src/main/java/com/inertia/lockersapi/api/controller/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
@@ -27,6 +29,11 @@ public class UserController {
     @GetMapping
     public ResponseEntity<?> getAllUsers() {
         return userService.findAllUsers();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<?> getUserById(@PathVariable UUID id) {
+        return ResponseEntity.ok(userService.findUserById(id));
     }
 
 
