abf044cf5f41692c55ebd5815a3fd7ad2cf19843
Luiz Henrique Netto <101609888+LuizHBN@users.noreply.github.com>
2025-09-18 19:44:20 -0300

    Get RentRequestByUserID now returning Locker Object

--- src/main/java/com/inertia/lockersapi/api/controller/dto/response/ReadRentRequestDTO.java
+++ src/main/java/com/inertia/lockersapi/api/controller/dto/response/ReadRentRequestDTO.java
@@ -1,15 +1,16 @@
 package com.inertia.lockersapi.api.controller.dto.response;
 
+import com.inertia.lockersapi.domain.locker.Locker;
 import com.inertia.lockersapi.domain.rentRequest.RentRequest;
 
 import java.util.Date;
 import java.util.UUID;
 
-public record ReadRentRequestDTO(UUID rentRequestId,UUID lockerId, UUID userId, Date rentStartDate, Date rentFinishDate,double amount, UUID openingKey) {
+public record ReadRentRequestDTO(UUID rentRequestId, Locker locker, UUID userId, Date rentStartDate, Date rentFinishDate, double amount, UUID openingKey) {
     public ReadRentRequestDTO(RentRequest rentRequest) {
         this(
                 rentRequest.getId(),
-                rentRequest.getLocker() != null ? rentRequest.getLocker().getId() : null,
+                rentRequest.getLocker() != null ? rentRequest.getLocker() : null,
                 rentRequest.getUser() != null ? rentRequest.getUser().getId() : null,
                 rentRequest.getRentStartDate(),
                 rentRequest.getRentFinishDate(),
